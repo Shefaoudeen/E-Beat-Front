e3a8239dfe684aa4614e270906263b70796d9f9a
Vishnou166 <147917011+Vishnou166@users.noreply.github.com>
2023-10-15 20:14:41 +0530

    Add files via upload

---
+++ 18.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+/*import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/utils.dart';*/
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Scene(),
+        ),
+      ),
+    );
+  }
+}
+
+class Scene extends StatelessWidget {
+  const Scene({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double baseWidth = 360;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
+    return SizedBox(
+      width: double.infinity,
+      child: TextButton(
+        // welcomepagesy3 (1:2)
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+        ),
+        child: Container(
+          padding:
+              EdgeInsets.fromLTRB(63 * fem, 380 * fem, 107 * fem, 76 * fem),
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            color: Color(0xff033ba6),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                'assets/page-1/images/rectangle-1-bg.png',
+              ),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                // ebeatyYD (1:3)
+                margin:
+                    EdgeInsets.fromLTRB(0 * fem, 0 * fem, 34 * fem, 232 * fem),
+                child: Text(
+                  '#E-BEAT',
+                  style: TextStyle(
+                    fontFamily: 'Nico Moji',
+                    fontSize: 40 * ffem,
+                    fontWeight: FontWeight.w400,
+                    height: 1.2575 * ffem / fem,
+                    letterSpacing: 2.4 * fem,
+                    color: const Color(0xffffffff),
+                  ),
+                ),
+              ),
+              Container(
+                // group2Cvm (1:6)
+                margin:
+                    EdgeInsets.fromLTRB(34 * fem, 0 * fem, 0 * fem, 0 * fem),
+                width: 156 * fem,
+                height: 61 * fem,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      // rectangle18uqB (1:7)
+                      left: 0 * fem,
+                      top: 0 * fem,
+                      child: Align(
+                        child: SizedBox(
+                          width: 150.13 * fem,
+                          height: 61 * fem,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Color(0xfffefefe),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      // rectangle17cDo (1:8)
+                      left: 1.9142227173 * fem,
+                      top: 0 * fem,
+                      child: Align(
+                        child: SizedBox(
+                          width: 38.92 * fem,
+                          height: 61 * fem,
+                          child: Image.asset(
+                            'assets/page-1/images/rectangle-17.png',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      // rectangle19jJR (1:9)
+                      left: 43.0696563721 * fem,
+                      top: 0 * fem,
+                      child: Align(
+                        child: SizedBox(
+                          width: 0.64 * fem,
+                          height: 61 * fem,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Color(0xff000000),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      // puducherrypolicegovernmentofpu (1:10)
+                      left: 46 * fem,
+                      top: 6 * fem,
+                      child: Align(
+                        child: SizedBox(
+                          width: 110 * fem,
+                          height: 51 * fem,
+                          child: Text(
+                            'Puducherry Police\nGovernment of Puducherry ',
+                            style: TextStyle(
+                              fontFamily: 'Odor Mean Chey',
+                              fontSize: 12 * ffem,
+                              fontWeight: FontWeight.w400,
+                              height: 1.4166666667 * ffem / fem,
+                              letterSpacing: 0.72 * fem,
+                              color: const Color(0xff040404),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ 2.dart
@@ -0,0 +1,380 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter/gestures.dart';
+//import 'dart:ui';
+//import 'package:google_fonts/google_fonts.dart';
+//import 'package:myapp/utils.dart';
+
+//import 'package:myapp/page-1/log-in-page.dart';
+//import 'package:myapp/page-1/home-db.dart';
+// import 'package:myapp/page-1/home-tracking.dart';
+// import 'package:myapp/page-1/profile.dart';
+// import 'package:myapp/page-1/notifications.dart';
+// import 'package:myapp/page-1/directory.dart';
+// import 'package:myapp/page-1/assign-page-1.dart';
+// import 'package:myapp/page-1/report-page-1.dart';
+// import 'package:myapp/page-1/report-page-2.dart';
+// import 'package:myapp/page-1/view-page-1.dart';
+// import 'package:myapp/page-1/assign-page-2.dart';
+// import 'package:myapp/page-1/view-page-2.dart';
+// import 'package:myapp/page-1/assign-page-3.dart';
+// import 'package:myapp/page-1/assign-page-4.dart';
+// import 'package:myapp/page-1/assign-page-5.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Scene(),
+        ),
+      ),
+    );
+  }
+}
+
+class Scene extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double baseWidth = 360;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
+    return Container(
+      width: double.infinity,
+      child: Container(
+        // assignpage2fjP (4:17)
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Color(0xffffffff),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              // rectangle36CUR (4:18)
+              width: double.infinity,
+              height: 25 * fem,
+              decoration: const BoxDecoration(
+                color: Color(0xffd9d9d9),
+              ),
+            ),
+            Container(
+              // autogroupvgtsYYH (XmYDbn1d1rw853Hk1qVgTs)
+              padding:
+                  EdgeInsets.fromLTRB(21 * fem, 22 * fem, 13 * fem, 15 * fem),
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Color(0xff8b98d8),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    // frame16ros (4:26)
+                    margin: EdgeInsets.fromLTRB(
+                        0 * fem, 0 * fem, 9.03 * fem, 8 * fem),
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: Container(
+                        width: 29.97 * fem,
+                        height: 30 * fem,
+                        child: Image.asset(
+                          'assets/page-1/images/frame-16-Nth.png',
+                          width: 29.97 * fem,
+                          height: 30 * fem,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    // ebeat9HB (4:21)
+                    margin: EdgeInsets.fromLTRB(
+                        0 * fem, 0 * fem, 102 * fem, 0 * fem),
+                    child: Text(
+                      '#E-BEAT',
+                      style: TextStyle(
+                        fontFamily: 'Nico Moji',
+                        fontSize: 30 * ffem,
+                        fontWeight: FontWeight.w400,
+                        height: 1.2575 * ffem / fem,
+                        letterSpacing: 1.8 * fem,
+                        color: const Color(0xff000000),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    // frame15ezd (4:24)
+                    margin: EdgeInsets.fromLTRB(
+                        0 * fem, 0 * fem, 10.5 * fem, 8 * fem),
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: Container(
+                        width: 27.5 * fem,
+                        height: 30 * fem,
+                        child: Image.asset(
+                          'assets/page-1/images/frame-15-fff.png',
+                          width: 27.5 * fem,
+                          height: 30 * fem,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    // framemJZ (4:22)
+                    margin:
+                        EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 8 * fem),
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: Container(
+                        width: 30 * fem,
+                        height: 30 * fem,
+                        child: Image.asset(
+                          'assets/page-1/images/frame-1ay.png',
+                          width: 30 * fem,
+                          height: 30 * fem,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              // autogrouparer4YZ (XmYEzuM7QpA1y9zXBoaRER)
+              padding:
+                  EdgeInsets.fromLTRB(5 * fem, 15 * fem, 5 * fem, 200 * fem),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    // autogroupfxzhyvR (XmYDqX7iqaSkyAG5LcfXzH)
+                    width: double.infinity,
+                    height: 50 * fem,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffbbc7e7),
+                      borderRadius: BorderRadius.circular(5 * fem),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'BEAT-X',
+                        style: TextStyle(
+                          fontFamily: 'Sen',
+                          fontSize: 25 * ffem,
+                          fontWeight: FontWeight.w400,
+                          height: 1.2025 * ffem / fem,
+                          letterSpacing: 1.5 * fem,
+                          color: const Color(0xff000f97),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15 * fem,
+                  ),
+                  Container(
+                    // autogroup14zdSJD (XmYDwgc7qXyGsiDDet14zD)
+                    width: double.infinity,
+                    height: 50 * fem,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffbbc7e7),
+                      borderRadius: BorderRadius.circular(50 * fem),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Select Beat-subdivisions',
+                        style: TextStyle(
+                          fontFamily: 'Sen',
+                          fontSize: 20 * ffem,
+                          fontWeight: FontWeight.w400,
+                          height: 1.2025 * ffem / fem,
+                          letterSpacing: 1.2 * fem,
+                          color: const Color(0xff000f97),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15 * fem,
+                  ),
+                  Container(
+                    // autogroupoob7hzq (XmYE4Wk57ARFXZiyMVooB7)
+                    margin: EdgeInsets.fromLTRB(
+                        10 * fem, 0 * fem, 9.5 * fem, 0 * fem),
+                    width: double.infinity,
+                    height: 100 * fem,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          // autogroups7sh2XK (XmYECWWkMDUnVCUHyjS7Sh)
+                          margin: EdgeInsets.fromLTRB(
+                              0 * fem, 0 * fem, 15.5 * fem, 0 * fem),
+                          width: 157.5 * fem,
+                          height: double.infinity,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffbbc7e7),
+                            borderRadius: BorderRadius.circular(10 * fem),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'SD-1',
+                              style: TextStyle(
+                                fontFamily: 'Sen',
+                                fontSize: 25 * ffem,
+                                fontWeight: FontWeight.w400,
+                                height: 1.2025 * ffem / fem,
+                                letterSpacing: 1.5 * fem,
+                                color: const Color(0xffffffff),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          // autogroup8uhfhdT (XmYEGLjhQKnBnBxJSm8UHF)
+                          width: 157.5 * fem,
+                          height: double.infinity,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffbbc7e7),
+                            borderRadius: BorderRadius.circular(10 * fem),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'SD-2',
+                              style: TextStyle(
+                                fontFamily: 'Sen',
+                                fontSize: 25 * ffem,
+                                fontWeight: FontWeight.w400,
+                                height: 1.2025 * ffem / fem,
+                                letterSpacing: 1.5 * fem,
+                                color: const Color(0xffffffff),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15 * fem,
+                  ),
+                  Container(
+                    // autogroupuudtAmw (XmYEXfdVckynx9tLJsuudT)
+                    margin: EdgeInsets.fromLTRB(
+                        10 * fem, 0 * fem, 9.5 * fem, 0 * fem),
+                    width: double.infinity,
+                    height: 100 * fem,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          // autogroupqs7bJNM (XmYEefRqTJf8nqHjraqS7b)
+                          margin: EdgeInsets.fromLTRB(
+                              0 * fem, 0 * fem, 15.5 * fem, 0 * fem),
+                          width: 157.5 * fem,
+                          height: double.infinity,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffbbc7e7),
+                            borderRadius: BorderRadius.circular(10 * fem),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'SD-3',
+                              style: TextStyle(
+                                fontFamily: 'Sen',
+                                fontSize: 25 * ffem,
+                                fontWeight: FontWeight.w400,
+                                height: 1.2025 * ffem / fem,
+                                letterSpacing: 1.5 * fem,
+                                color: const Color(0xffffffff),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          // autogroupmpydBS9 (XmYEipyZeFRGTUZ41nmPYd)
+                          width: 157.5 * fem,
+                          height: double.infinity,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffbbc7e7),
+                            borderRadius: BorderRadius.circular(10 * fem),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'SD-4',
+                              style: TextStyle(
+                                fontFamily: 'Sen',
+                                fontSize: 25 * ffem,
+                                fontWeight: FontWeight.w400,
+                                height: 1.2025 * ffem / fem,
+                                letterSpacing: 1.5 * fem,
+                                color: const Color(0xffffffff),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15 * fem,
+                  ),
+                  Container(
+                    // autogroupiwnmTPf (XmYEraFf2vdkdsTtsPiWNM)
+                    margin:
+                        EdgeInsets.fromLTRB(9 * fem, 0 * fem, 0 * fem, 0 * fem),
+                    width: 157.5 * fem,
+                    height: 100 * fem,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffbbc7e7),
+                      borderRadius: BorderRadius.circular(10 * fem),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'SD-5',
+                        style: TextStyle(
+                          fontFamily: 'Sen',
+                          fontSize: 25 * ffem,
+                          fontWeight: FontWeight.w400,
+                          height: 1.2025 * ffem / fem,
+                          letterSpacing: 1.5 * fem,
+                          color: const Color(0xffffffff),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              // rectangle37jc5 (4:19)
+              width: double.infinity,
+              height: 25 * fem,
+              decoration: const BoxDecoration(
+                color: Color(0xffd9d9d9),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
